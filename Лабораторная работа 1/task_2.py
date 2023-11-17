001d2bf704f244c5b9e86b334940e3e59d1e8235
vasbar <149441143+vasbar@users.noreply.github.com>
2023-11-17 19:46:16 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,7 +1,10 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-book = 4*25*50*100/1024
-#print(book)
-disk=1.44*1024
-#print(disk)
-cnt=int(disk/book)
+pages = 100
+strings = 50
+symbols = 25
+kb = 1024
+bty = 4
+disk_vol = 1.44
+volume = symbols * strings * pages * bty/kb
+cnt = int((disk_vol * kb // volume))
 print("Количество книг, помещающихся на дискету:", cnt)
