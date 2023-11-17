d2ce9b0d7b3303220593b20e7768ca56800f8e16
vasbar <149441143+vasbar@users.noreply.github.com>
2023-11-17 19:45:29 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,6 +1,9 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
+missing_index = 4
+numbers[missing_index] = 0
+summa = sum(numbers)
+avg = summa/(len(numbers))
+numbers[missing_index] = avg
 # TODO заменить значение пропущенного элемента средним арифметическим
-summa=sum(numbers[0:4])+sum(numbers[5:])
-numbers[4]=summa/(len(numbers))
+
 print("Измененный список:", numbers)
