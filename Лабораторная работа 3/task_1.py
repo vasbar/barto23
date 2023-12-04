912b56dfb1e339d6184b18535c30f79c4aa71308
vasbar <149441143+vasbar@users.noreply.github.com>
2023-12-04 19:41:07 +0300

    Update task_1.py

--- Лабораторная работа 3/task_1.py
+++ Лабораторная работа 3/task_1.py
@@ -3,14 +3,14 @@ def idxs(spis, tovar):
     for i in range(len(spis)):
         if tovar == spis[i]:
             return i
-        if i == len(spis):
+        if i == (len(spis) - 1):
             return None
 
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 for find_item in ['банан', 'груша', 'персик']:
     index_item = idxs(items_list, find_item)
-    if index_item is None:
-        print(f"Товар '{find_item}' не найден в списке.")
-    else:
+    if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
