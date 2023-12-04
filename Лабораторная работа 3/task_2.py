3c9ab6c683e96989562b7d32d5b926c7a453ba3a
vasbar <149441143+vasbar@users.noreply.github.com>
2023-12-04 19:49:14 +0300

    Update task_2.py

--- Лабораторная работа 3/task_2.py
+++ Лабораторная работа 3/task_2.py
@@ -1,7 +1,7 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(spis1, spis2, delimeter=","):
     spis1 = set(spis1.split(delimeter))
-    spis2 = set(spis2.split(delimeter))
+    spis2 = spis2.split(delimeter)
     res = spis1.intersection(spis2)
     res = list(res)
     res.sort()
@@ -10,6 +10,6 @@ def find_common_participants(spis1, spis2, delimeter=","):
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-print(find_common_participants(participants_first_group,participants_second_group,"|"))
+print(find_common_participants(participants_first_group,participants_second_group, "|"))
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
